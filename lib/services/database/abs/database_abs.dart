30d616cc3211bd51ebf98a0b33bd8a4c2200c237
Ratakondala Arun <arunratakondala3470@gmail.com>
2020-12-17 19:45:46 +0530

    refactred database service to singleton instance

--- lib/services/database/abs/database_abs.dart
+++ lib/services/database/abs/database_abs.dart
@@ -1,16 +1,23 @@
 library services.database;
 
+import 'package:gitter/services/database/database.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart' as pp;
+
+import 'package:gitterapi/gitter_api.dart';
 import 'package:gitterapi/models.dart';
 
 part '../error/database_service_exception.dart';
 part '../offline_database.dart';
+part '../online_database.dart';
 
 /// Base class for DataBaseService
 abstract class DatabaseServiceAbs {
   /// To Make sure that everything is initilized
   bool get isInitilized;
+
+  /// Stores Creditionals.
+  CreditionalDatabase get creditional;
 
   /// Operations on current user.
   CurrentUserDatabase get currentUser;
@@ -39,3 +46,85 @@ abstract class CurrentUserDatabase {
   /// Deletes current user.
   Future<void> delete();
 }
+
+abstract class CreditionalDatabase {
+  Future<void> create(String name, String value);
+  Future<String> get(String name);
+  Future<void> update(String name, String value);
+  Future<void> delete(String name);
+}
+
+abstract class UserResourceDatabase {}
+
+/// Base class for all creditionals
+// abstract class CreditionalItem {
+//   /// This should be a unique name.
+//   ///
+//   /// Preffered class name as snakecase. Must not be null.
+//   /// ```
+//   /// String get name => 'creditional_item';
+//   /// ```
+//   String get name;
+
+//   /// Returns `lastUpdated` in [DateTime] format.
+//   DateTime get lastUpdatedAs => DateTime.tryParse(lastUpdated);
+
+//   /// Last updated time in UTC and stored in String format.
+//   final String lastUpdated;
+
+//   /// Creates a instance.
+//   CreditionalItem(this.lastUpdated);
+
+//   /// Returns [Map<String,dynamic>]
+//   Map<String, dynamic> toMap();
+// }
+
+// /// This CreditionalItem holds access_token
+// class AccessTokenCItem extends CreditionalItem {
+//   final String accessToken;
+
+//   @override
+//   String get name => 'access_token_creditional';
+
+//   AccessTokenCItem({
+//     this.accessToken,
+//     String lastUpdated,
+//   }) : super(lastUpdated);
+
+//   factory AccessTokenCItem.create(String accessToken) {
+//     return AccessTokenCItem(
+//       accessToken: accessToken,
+//       lastUpdated: DateTime.now().toUtc().toString(),
+//     );
+//   }
+
+//   factory AccessTokenCItem.fromMap(Map map) {
+//     return AccessTokenCItem(
+//       accessToken: map['accessToken'],
+//       lastUpdated: map['lastUpdated'],
+//     );
+//   }
+
+//   @override
+//   Map<String, dynamic> toMap() {
+//     return {
+//       'accessToken': accessToken,
+//       'lastUpdated': lastUpdated,
+//     };
+//   }
+
+//   AccessTokenCItem copyWith({
+//     String accessToken,
+//     String lastUpdated,
+//   }) {
+//     return AccessTokenCItem(
+//       accessToken: accessToken ?? this.accessToken,
+//       lastUpdated: lastUpdated ?? this.lastUpdated,
+//     );
+//   }
+
+//   @override
+//   String toString() {
+//     return 'Creditional(lastUpdated: $lastUpdated, accessToken: $accessToken)';
+//   }
+// }
--- lib/services/database/database.dart
+++ lib/services/database/database.dart
@@ -1,3 +1,5 @@
+import 'package:gitterapi/models/user.dart';
+
 import 'abs/database_abs.dart';
 
 export 'abs/database_abs.dart'
@@ -6,6 +8,14 @@ export 'abs/database_abs.dart'
 class DatabaseService implements DatabaseServiceAbs {
   bool _isInitilized = false;
   DatabaseServiceAbs _offlineDB;
+  DatabaseServiceAbs _onlineDb;
+  CreditionalDatabase _creditionalDB;
+  CurrentUserDatabase _currentUserDB;
+
+  static DatabaseService _instance = DatabaseService._();
+
+  /// Returns a singleton instance of [DatabaseService].
+  static DatabaseService get instance => _instance;
 
   @override
   bool get isInitilized => _isInitilized;
@@ -17,22 +27,72 @@ class DatabaseService implements DatabaseServiceAbs {
         'NOT_INITILIZED, Call init() method before using this.',
       );
     }
-    return _offlineDB.currentUser;
+    return _currentUserDB;
   }
 
-  DatabaseService() {
+  @override
+  CreditionalDatabase get creditional => _creditionalDB;
+
+  DatabaseService._() {
     init();
   }
 
   @override
   Future<void> init() async {
-    _offlineDB = OfflineDatabaseService();
+    // Must be initilized before initilizing others
+    _offlineDB = OfflineDatabaseService.instance;
+    _onlineDb = OnlineDatabaseService.instance;
     await _offlineDB.init();
-    _isInitilized = true && _offlineDB.isInitilized;
+    _creditionalDB = _offlineDB.creditional;
+    await _onlineDb.init();
+
+    _currentUserDB = _CurrentUserService(
+      _onlineDb.currentUser,
+      _offlineDB.currentUser,
+    );
+    _isInitilized = true && _offlineDB.isInitilized && _onlineDb.isInitilized;
   }
 
   @override
   Future<void> close() {
     return _offlineDB.close();
+  }
+}
+
+class _CurrentUserService extends CurrentUserDatabase {
+  final CurrentUserDatabase _onlineDb;
+  final CurrentUserDatabase _offlineDb;
+
+  _CurrentUserService(this._onlineDb, this._offlineDb);
+
+  @override
+  Stream<User> get currentUserChanges => _offlineDb.currentUserChanges;
+
+  @override
+  Future<void> create(User user) {
+    return _offlineDb.create(user);
+  }
+
+  @override
+  Future<void> delete() {
+    return _offlineDb.delete();
+  }
+
+  @override
+  Future<User> get() async {
+    User user;
+    try {
+      // if failed to get user from online db fetch it from offline
+      user = await _onlineDb.get();
+      return user;
+    } catch (e) {
+      print(e);
+    }
+    return _offlineDb.get();
+  }
+
+  @override
+  Future<void> update(User user) {
+    return _offlineDb.update(user);
   }
 }
